2e7abc74b4f418c6b24a53fb4712591d46b7c546
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2021-06-23 22:56:12 +0530

    description of project

--- src/App.js
+++ src/App.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./styles.css";
 
 export default function () {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <h1>🥗Delicious Cuisine</h1>
+        <p>You can check out this and select your favourite food type</p>
+      </div>
+    </div>
+  );
 }
