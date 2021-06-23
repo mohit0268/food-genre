d8fab5f0061f5172884616eb027a959cb3983839
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2021-06-23 23:13:48 +0530

    made a database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function () {
+  const foodDb = {
+    Indian: [
+      { name: "Paneer masala", ratings: "4.5/5" },
+      { name: "Samosa", ratings: "4/5" }
+    ],
+    Chinese: [
+      { name: "Noodles", ratings: "3.5/5" },
+      { name: "Spring Roll", ratings: "4/5" }
+    ],
+    Italan: [
+      { name: "Pizza", ratings: "4.5/5" },
+      { name: "Pasta", ratings: "4/5" }
+    ]
+  };
+
   return (
     <div className="App">
       <div className="container-fluid">
